1b16d674f4b5577fbd3d70abd9751eb2ce00616b
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-03-19 20:51:25 -0300

    Add loop para multiplas tentativas

--- Advinhacao.ConsoleApp/Program.cs
+++ Advinhacao.ConsoleApp/Program.cs
@@ -2,29 +2,44 @@
 
 // JOGO DE ADVINHAÇÃO
 
-
-Console.WriteLine("------------------------------");
-Console.WriteLine("JOGO DE ADVINHAÇÃO");
-Console.WriteLine("------------------------------");
-
-// 1- Nosso jogo deve aceitar o input do jogador e exibir o valor digitado
-Console.WriteLine();
-Console.Write("Digite um número: ");
-int numdig = Convert.ToInt32(Console.ReadLine());
-
 int numaleatorio = RandomNumberGenerator.GetInt32(1, 21);
+bool cont = true;
 
-if (numdig == numaleatorio)
+while (cont == true)
 {
-    Console.WriteLine("Muito bem. Você acertou o número secreto, que era: " + numaleatorio);
-}
-else if (numdig > numaleatorio)
-{
-    Console.WriteLine("O número que você digitou é maior que o número secreto");
-}
-else
-{
-    Console.WriteLine("O número que você digitou é menor que o número secreto");
+    Console.Clear();
+    Console.WriteLine("------------------------------");
+    Console.WriteLine("JOGO DE ADVINHAÇÃO");
+    Console.WriteLine("------------------------------");
+
+    Console.WriteLine();
+    Console.Write("Digite um número: ");
+    int numdig = Convert.ToInt32(Console.ReadLine());
+
+
+    if (numdig == numaleatorio)
+    {
+        Console.WriteLine("Muito bem. Você acertou o número secreto, que era: " + numaleatorio);
+    }
+    else if (numdig > numaleatorio)
+    {
+        Console.WriteLine("O número que você digitou é maior que o número secreto");
+    }
+    else
+    {
+        Console.WriteLine("O número que você digitou é menor que o número secreto");
+    }
+
+    Console.WriteLine();
+    Console.Write("Pressione S para continuar ou N para sair: ");
+    string opcaocont = Console.ReadLine();
+
+    if (opcaocont != "S" && opcaocont != "s")
+    {
+        cont = false;
+    }
+
+    Console.ReadLine();
+
 }
 
-Console.ReadLine();
