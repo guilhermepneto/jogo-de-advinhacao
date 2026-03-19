73b01e41b04925745831104cfccd57862638d455
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-03-19 19:52:19 -0300

    Add input e output

--- Advinhacao.ConsoleApp/Program.cs
+++ Advinhacao.ConsoleApp/Program.cs
@@ -1,3 +1,15 @@
-﻿
+﻿// JOGO DE ADVINHAÇÃO
 
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("JOGO DE ADVINHAÇÃO");
+Console.WriteLine("------------------------------");
+
+// 1- Nosso jogo deve aceitar o input do jogador e exibir o valor digitado
+Console.WriteLine();
+Console.Write("Digite um número: ");
+string strnumdig = Console.ReadLine();
+
+Console.WriteLine("O número digitado foi: " + strnumdig);
+
+Console.ReadLine();
