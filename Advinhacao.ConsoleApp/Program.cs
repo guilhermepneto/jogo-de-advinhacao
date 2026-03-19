6d3f56b78616e3c527f3ab4760927010d5c31ac4
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-03-19 20:31:33 -0300

    Add condicionais para comparar numero digitado com o numero aleatorio

--- Advinhacao.ConsoleApp/Program.cs
+++ Advinhacao.ConsoleApp/Program.cs
@@ -10,11 +10,21 @@ Console.WriteLine("------------------------------");
 // 1- Nosso jogo deve aceitar o input do jogador e exibir o valor digitado
 Console.WriteLine();
 Console.Write("Digite um número: ");
-string strnumdig = Console.ReadLine();
-
+int numdig = Convert.ToInt32(Console.ReadLine());
 
 int numaleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
-Console.WriteLine("O número aleatório foi: " + numaleatorio);
+if (numdig == numaleatorio)
+{
+    Console.WriteLine("Muito bem. Você acertou o número secreto, que era: " + numaleatorio);
+}
+else if (numdig > numaleatorio)
+{
+    Console.WriteLine("O número que você digitou é maior que o número secreto");
+}
+else
+{
+    Console.WriteLine("O número que você digitou é menor que o número secreto");
+}
 
 Console.ReadLine();
